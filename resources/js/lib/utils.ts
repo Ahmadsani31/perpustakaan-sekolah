dd2e0e1f43af2bbcec1a9ea826b60303345a33ff
Ahmadsani31 <ahmaddarma0@gmail.com>
2025-06-21 21:55:08 +0700

    fix minor & add categories

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,51 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function flashMessage(params: any) {
+    return params.props.flash_message;
+}
+
+export function formatToRupiah(amount: any) {
+    const formatter = new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
+
+    return formatter.format(amount);
+}
+
+export const message = {
+    503: {
+        title: 'Service Unavailable',
+        description: 'Sorry, we are doing some maintenance. Please check back soon',
+        status: '503'
+    },
+    500: {
+        title: 'Server Error',
+        description: 'Oops, something went wrong on our server',
+        status: '500'
+    },
+    404: {
+        title: 'Not Found',
+        description: 'Sorry, the page you are looking for could not be found',
+        status: '404'
+    },
+    403: {
+        title: 'Forbidden',
+        description: 'Sorry, you are forbidden from a accessing this page',
+        status: '403'
+    },
+    401: {
+        title: 'Unauthorized',
+        description: 'Sorry, you are unauthorized to access this page',
+        status: '401'
+    },
+    429: {
+        title: 'Too Many Request',
+        description: 'please try again in just a second',
+        status: '429'
+    },
+}
